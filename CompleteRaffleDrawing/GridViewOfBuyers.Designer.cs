537605e0c99f87aa8cbb623239ef4a7fb63dc9b1
Rick <prefect12@gmail.com>
2015-08-30 15:15:21 -0700

    Added new form for checking on tickets bought by individuals.  Still need to work on placing the connection string into the code for it and not into the app.config file.

---
+++ CompleteRaffleDrawing/GridViewOfBuyers.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace CompleteRaffleDrawing
+{
+    partial class GridViewOfBuyers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GridViewOfBuyers));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ticketBuyerInfo = new CompleteRaffleDrawing.TicketBuyerInfo();
+            this.ticketBuyerBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.ticketBuyerTableAdapter = new CompleteRaffleDrawing.TicketBuyerInfoTableAdapters.TicketBuyerTableAdapter();
+            this.buyerNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ticketsBoughtDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bonusTicketsDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ticketBuyerInfo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ticketBuyerBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.buyerNameDataGridViewTextBoxColumn,
+            this.ticketsBoughtDataGridViewTextBoxColumn,
+            this.bonusTicketsDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.ticketBuyerBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(346, 237);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // ticketBuyerInfo
+            // 
+            this.ticketBuyerInfo.DataSetName = "TicketBuyerInfo";
+            this.ticketBuyerInfo.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // ticketBuyerBindingSource
+            // 
+            this.ticketBuyerBindingSource.DataMember = "TicketBuyer";
+            this.ticketBuyerBindingSource.DataSource = this.ticketBuyerInfo;
+            // 
+            // ticketBuyerTableAdapter
+            // 
+            this.ticketBuyerTableAdapter.ClearBeforeFill = true;
+            // 
+            // buyerNameDataGridViewTextBoxColumn
+            // 
+            this.buyerNameDataGridViewTextBoxColumn.DataPropertyName = "BuyerName";
+            this.buyerNameDataGridViewTextBoxColumn.HeaderText = "BuyerName";
+            this.buyerNameDataGridViewTextBoxColumn.Name = "buyerNameDataGridViewTextBoxColumn";
+            // 
+            // ticketsBoughtDataGridViewTextBoxColumn
+            // 
+            this.ticketsBoughtDataGridViewTextBoxColumn.DataPropertyName = "TicketsBought";
+            this.ticketsBoughtDataGridViewTextBoxColumn.HeaderText = "TicketsBought";
+            this.ticketsBoughtDataGridViewTextBoxColumn.Name = "ticketsBoughtDataGridViewTextBoxColumn";
+            // 
+            // bonusTicketsDataGridViewTextBoxColumn
+            // 
+            this.bonusTicketsDataGridViewTextBoxColumn.DataPropertyName = "BonusTickets";
+            this.bonusTicketsDataGridViewTextBoxColumn.HeaderText = "BonusTickets";
+            this.bonusTicketsDataGridViewTextBoxColumn.Name = "bonusTicketsDataGridViewTextBoxColumn";
+            // 
+            // GridViewOfBuyers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 262);
+            this.Controls.Add(this.dataGridView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "GridViewOfBuyers";
+            this.Text = "Buyer Data";
+            this.Load += new System.EventHandler(this.GridViewOfBuyers_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ticketBuyerInfo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ticketBuyerBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private TicketBuyerInfo ticketBuyerInfo;
+        private System.Windows.Forms.BindingSource ticketBuyerBindingSource;
+        private TicketBuyerInfoTableAdapters.TicketBuyerTableAdapter ticketBuyerTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn buyerNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ticketsBoughtDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bonusTicketsDataGridViewTextBoxColumn;
+    }
+}
---
+++ CompleteRaffleDrawing/GridViewOfBuyers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CompleteRaffleDrawing
+{
+    public partial class GridViewOfBuyers : Form
+    {
+        public GridViewOfBuyers()
+        {
+            InitializeComponent();
+        }
+
+        private void GridViewOfBuyers_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'ticketBuyerInfo.TicketBuyer' table. You can move, or remove it, as needed.
+            this.ticketBuyerTableAdapter.Fill(this.ticketBuyerInfo.TicketBuyer);
+
+        }
+    }
+}
